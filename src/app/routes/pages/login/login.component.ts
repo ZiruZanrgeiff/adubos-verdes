7e650d8f1a49f42557cd0901884d177b1acd59a8
ZiruZanrgeiff <ronildo_net@hotmail.com>
2017-12-21 17:33:45 -0200

    testa a codacy

--- src/app/routes/pages/login/login.component.ts
+++ src/app/routes/pages/login/login.component.ts
@@ -11,6 +11,17 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    for(let i = 10; i < 100; i++) {
+      console.log('teste');
+      for(let a = 10; a < 100; i++) {
+        console.log('teste');
+        if (a ==1) {
+          for(let b = 10; b < 100; i++) {
+            console.log('teste');
+          }
+        }
+      }
+    }
   }
 
 }
